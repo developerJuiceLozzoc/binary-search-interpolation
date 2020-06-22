000fbddf42b6261606cfd9aff5df7b0be63d9aac
lozzoc <reach.maddaloc@gmail.com>
2020-06-22 15:46:50 -0700

    added proper names to units files

--- main.cpp
+++ main.cpp
@@ -51,83 +51,13 @@ float lookupTable(float * nums,int len, float searchTerm){
 }
 
 
-/* This table scans the data and performs binary search
-to find the closest match, then uses interpolate to return the value;
-in this case my table finds the y for the closest matching x.
-*/
-float lookupTableBST(float * nums,int len, float searchTerm){
-
-  /* First our bounds check, if the
-  value is outside the table,
-  return the y value from the edge*/
-
-  if(nums[0] >= searchTerm){
-    return nums[1];
-  }
-  else if(nums[len-2] <= searchTerm){
-    return nums[len-1];
-  }
-  else{
-    int mid,low=0, high=(len/2)-1;
-
-    while(low <= high){
-      mid = (low+high) /2;
-
-      if(nums[2*mid] == searchTerm){
-        return nums[2*mid+1];
-      }
-      /*====================== interpolate extra conditionals
-       */
-        // if the x values are wrappers
-      else if(mid!=0 && nums[2*mid] > searchTerm && nums[2*mid-2] < searchTerm ){
-        float x1 = nums[2*mid-2],
-              x3 = nums[2*mid];
-        float y1 = nums[2*mid-1],
-              y3 = nums[2*mid+1];
-
-        return (searchTerm - x1) * (y3-y1) / (x3-x1) + y1;
-      }
-      else if(mid!=len-2 && nums[2*mid] < searchTerm && nums[2*mid+2] > searchTerm ){
-        // x2 = nums[2*mid+2]
-        // x1 = nums[2*mid]
-        float x1 = nums[2*mid],
-              x3 = nums[2*mid+2];
-        float y1 = nums[2*mid+1],
-              y3 = nums[2*mid+3];
-        return (searchTerm - x1) * (y3-y1) / (x3-x1) + y1;
-      }
-      /*
-      basically if the in between value is to the right or left of our current
-      Cursor.  ~O(2) check.
-      ==========================
-      */
-
-      /*here is the bst code that is for a sorted array
-      set the left or right bounds to the current middle
-      then recalculate the middle*/
-      else if(nums[2*mid] < searchTerm){
-        low = mid+1;
-      }
-      else{
-        high = mid - 1;
-      }
-
-    }
-
-    printf("We should never reach this code\n");
-    return -1;
-
-  }
-
-  //return math
-}
-
-
 
 float rand_FloatRange(float a, float b)
 {
     return ((b - a) * ((float)rand() / RAND_MAX)) + a;
 }
+
+#define ARRAY_SIZE 500000
 
 int main(int argc, char* argv[]){
   int NUM_TRIALS = 0;
@@ -139,7 +69,6 @@ int main(int argc, char* argv[]){
     NUM_TRIALS = 10;
 
   }
-  const int ARRAY_SIZE = 500000;
   float * hardcoded = (float *) malloc(sizeof(float)*ARRAY_SIZE*2);
   float * trials = (float * ) malloc(sizeof(float)*NUM_TRIALS);
   float y,xt,yt;
