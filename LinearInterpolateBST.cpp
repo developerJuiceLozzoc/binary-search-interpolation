5b335bc2b5d7f09dfb49344d2ce7a4ac5eabfcb1
Conner <50972261+lozzoc@users.noreply.github.com>
2020-06-22 14:31:46 -0700

    production changes

--- LinearInterpolateBST.cpp
+++ LinearInterpolateBST.cpp
@@ -1,5 +1,5 @@
 #include <math.h>
-#include <stdio.h>
+
 
 
 
@@ -67,20 +67,5 @@ float InterpolateYGivenXBST(float * array, int numpairs, float searchTerm) {
       return  0; //in the while loop, there should be no way the location is not found. because of the two boundrys up top.
   
   }
-
-}
-
-
-int main(){
-
-  float nums[14]= {1.0,2 , 2.0,4,3,8 ,4,16 ,5,32 , 6,64, 7,128};
-
-  printf("Can it return the value? 4: %.2f\n", InterpolateYGivenXBST(nums,7,4.5));
-  printf("Can it return the value? 6.5: %.2f\n", InterpolateYGivenXBST(nums,7,6.5));
-  printf("Can it return the value? 7.5: %.2f\n", InterpolateYGivenXBST(nums,7,7.5));
-
-
-
-
 
 }
